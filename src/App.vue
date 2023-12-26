f51588d9c51fb8112cc07245c047d497b15e97ed
ar_parvez <arparvez50@gmail.com>
2023-12-27 00:46:37 +0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Startingpoint from './components/Startingpoint.vue';
+</script>
+
+<template>
+  <Startingpoint/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Startingpoint.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+
+<div class="bg-slate-800 p-4 rounded text-left">
+  <h2 class="text-xl mb-2">Hello Vite X Vue X Tailwind</h2>
+  <p>Basic boilerplate of tailwind & Vue</p>
+</div>
+</template>
+<style scoped>
+
+</style>
